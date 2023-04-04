11eac162a78a02e5f917c779afafe990318ca807
w30r <mr.meorsyamil@gmail.com>
2023-04-04 22:31:53 +0800

    another one

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,5 +6,5 @@ import styles from "@/styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Hello</h1>;
+  return <h1>Hello, another one</h1>;
 }
