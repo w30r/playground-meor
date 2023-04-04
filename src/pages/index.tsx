622295bb5ae1c786b38ba1481c7537c6d1d9c0e1
w30r <mr.meorsyamil@gmail.com>
2023-04-04 22:42:33 +0800

    Installed TailwindCSS for next Project lmao
    salah buat tadi...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Hello, another one</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold underline">Hello, another one</h1>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,tsx,ts}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
